4628561ad0338a54fbc454c0827670e7415da6dc
Sourav Paul <souravpaulcse@gmail.com>
2018-03-07 20:43:44 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Max_date</title>
+    <style> 
+	input[type=date]{
+		background-color: lightblue;
+	    padding: 12px 20px;
+	    margin: 8px 0;
+	    box-sizing: border-box;
+	    border: 2px solid red;
+	    border-radius: 4px;
+	}
+	</style>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/jquery.bxslider.css">
+	<link rel="stylesheet" type="text/css" href="css/isotope.css" media="screen" />	
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
+	<link href="css/prettyPhoto.css" rel="stylesheet" />
+	<link href="css/style.css" rel="stylesheet" />	
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+	<header>		
+		<nav class="navbar navbar-default navbar-static-top" role="navigation">
+			<div class="navigation">
+				<div class="container">					
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse.collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<div class="navbar-brand">
+							<a href="index.html"><h1><span>Max</span>Date</h1></a>
+						</div>
+					</div>
+					
+					<div class="navbar-collapse collapse">							
+						<div class="menu">
+							<ul class="nav nav-tabs" role="tablist">
+								<li role="presentation"><a href="index.html" class="active">Home</a></li>						
+							</ul>
+						</div>
+					</div>						
+				</div>
+			</div>	
+		</nav>		
+	</header>
+   
+	<div class="slider">
+		<div class="img-responsive">
+			<ul class="bxslider">				
+				<li><img src="images/slider/1.jpg" alt=""/></li>								
+				<li><img src="images/slider/2.jpg" alt=""/></li>	
+				<li><img src="images/slider/3.jpg" alt=""/></li>			
+			</ul>
+		</div>	
+    </div>
+	
+	<div class="container">
+		<div class="text-center">
+			<div class="wow bounceInDown" data-wow-offset="0" data-wow-delay="0.3s">
+				<h3>Providing Our Clients</h3>
+			</div>
+			<div class="wow bounceInDown" data-wow-offset="0" data-wow-delay="0.6s">
+				<h2>Best & Creative Solutions</h2>
+			</div>
+		</div>
+	</div>
+	
+		<div class="box">
+			<div class="wow fadeInLeft" data-wow-offset="0" data-wow-delay="0.4s">
+				<div class="col-sm-3">
+					<i class="fa fa-cogs fa-3x"></i>
+					<h4>Settings</h4>
+				</div>
+			</div>
+			<div class="wow fadeInLeft" data-wow-offset="0" data-wow-delay="0.8s">
+				<div class="col-sm-3">
+					<i class="fa fa-leaf fa-3x"></i>
+					<h4>Creative Solutions</h4>
+				</div>
+			</div>
+			<div class="wow fadeInRight" data-wow-offset="0" data-wow-delay="1.2s">
+				<div class="col-sm-3">
+					<i class="fa fa-heart-o fa-3x"></i>
+					<h4>Make your Solutions</h4>
+				</div>
+			</div>
+			<div class="wow fadeInRight" data-wow-offset="0" data-wow-delay="1.6s">
+				<div class="col-sm-3">
+					<i class="fa fa-laptop fa-3x"></i>
+					<h4>Vulneraabilities</h4>
+				</div>
+			</div>
+		</div>
+	
+	<div class="features">
+		<div class="container">
+			<div class="text-center">
+				<div class="wow bounceInDown" data-wow-offset="0" data-wow-delay="0.3s">
+					<h2>Main Features</h2>
+				</div>
+				<div class="wow bounceInDown" data-wow-offset="0" data-wow-delay="0.6s">
+					<p>Everything You Know<br></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="container">
+		<div class="col-lg-6 wow fadeInRight" data-wow-offset="0" data-wow-delay="0.5s">
+			<img src="images/6.jpg" class="img-responsive" >
+		</div>
+		
+		<div class="col-lg-6 wow fadeInLeft" data-wow-offset="0" data-wow-delay="0.5s">
+			<img src="images/5.jpg" class="img-responsive" >
+		</div>
+	</div>
+	
+	<div class="main-feature">
+		<div class="container">
+			<div class="col-md-4 wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">
+				<div class="media">
+					<div class="media-left">					
+						<i class="fa fa-rocket fa-3x"></i>
+					</div>
+					
+					<div class="media-body">
+						<h4 class="media-heading">Optimized Date</h4>
+						<p>Make Your optimization.</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-md-4 wow fadeInUp" data-wow-offset="0" data-wow-delay="0.6s">
+				<div class="media">
+					<div class="media-left">					
+						<i class="fa fa-camera fa-3x"></i>
+					</div>
+					
+					<div class="media-body">
+						<h4 class="media-heading">Easy to get</h4>
+						<p>Know about your next</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-md-4 wow fadeInUp" data-wow-offset="0" data-wow-delay="0.9s">
+				<div class="media">
+					<div class="media-left">					
+						<i class="fa fa-heart-o fa-3x"></i>
+					</div>
+					
+					<div class="media-body">
+						<h4 class="media-heading">Unlimited Possibilities</h4>
+						<p>Get Every Possibilities</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="text-center">
+			<div class="wow bounceInDown" data-wow-offset="0" data-wow-delay="0.3s">
+				<h2>Sort out every moments </h2>
+			</div>
+			<div class="wow bounceInUp" data-wow-offset="0" data-wow-delay="0.6s">
+				<p>Sare your Things</p>
+			</div>
+			
+			
+			
+			<br>
+			<br>
+			
+		</div>	
+		
+	</div>
+
+	
+		<div id="initial">
+
+			<form align="center"  method="post" action="/parse/maxdat/logfile.php">
+			<div align="center" class="daterange">
+				<div>
+					<input type="date" name="startdate" style="text-align:center" placeholder="Start Date"></input>
+				</div>
+				<div>
+					<input type="date" name="enddate" style="text-align:center" placeholder="End Date"></input>
+				</div>
+				<div>
+					<input name="sbmt" type="submit"> 
+				</div>
+			</div>
+			</form>
+
+		</div>
+
+	
+	
+	<div class="about-us">
+		<div class="container">
+			<div class="text-center">
+				<div class="wow bounceInDown" data-wow-offset="0" data-wow-delay="0.3s">
+					<h3>Who are you</h3>
+				</div>
+				<div class="wow bounceInDown" data-wow-offset="0" data-wow-delay="0.6s">
+					<h2>Saying About Us</h2>
+				</div>
+				
+			</div>	
+		</div>
+	</div>
+
+
+	
+	<footer>
+		<div class="footer">
+			<div class="container">
+
+
+
+				<div class="col-md-4 wow fadeInUp" data-wow-offset="0" data-wow-delay="0.2s">
+					<h2>Max_ Date</h2>
+					<p>Its All about your date</p>
+					
+					<ul class="social-network">
+						<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook fa-1x"></i></a></li>
+						<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter fa-1x"></i></a></li>
+						<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin fa-1x"></i></a></li>
+						<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest fa-1x"></i></a></li>
+						<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus fa-1x"></i></a></li>
+					</ul>
+				</div>
+				
+				<div class="col-md-4 wow fadeInUp" data-wow-offset="0" data-wow-delay="0.4s">
+					<h3>RECENT POSTS</h3>
+					<ul>
+						<li>Think of days</li><hr>
+						<li>Sixty Summer of Days </li><hr>
+						<li>Long days of winter </li><hr>
+						<li>Days of multiple airways</li>
+					</ul>
+				</div>
+				
+				<div class="col-md-4 wow fadeInUp" data-wow-offset="0" data-wow-delay="0.6s">
+					<h3>CONTACT INFO</h3>
+					<ul>
+						<li><i class="fa fa-home fa-2x"></i>Gulshan, Dhaka</li><hr>
+						<li><i class="fa fa-phone fa-2x"></i> +88074979-7777</li><hr>
+						<li><i class="fa fa-envelope fa-2x"></i> sourav@info.net</li>
+					</ul>
+				</div>
+				
+			</div>
+		</div>
+		
+		<div class="sub-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						&copy; 2018 <a target="_blank" href="http://bootstraptaste.com/" title="Free Twitter Bootstrap WordPress Themes and HTML templates">bootstraptaste</a>. All Rights Reserved.
+					</div>
+                    <!-- 
+                        All links in the footer should remain intact. 
+                        Licenseing information is available at: http://bootstraptaste.com/license/
+                        You can buy this theme without footer links online at: http://bootstraptaste.com/buy/?theme=Multi
+                    -->
+					<div class="col-md-6">
+						<ul class="pull-right">
+							<li><a href="#">Home</a></li>
+							<li><a href="#">Services</a></li>
+							<li><a href="#">Blog</a></li>
+							<li><a href="#">Portfolio</a></li>
+							<li><a href="#">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="pull-right">
+					<a href="#home" class="scrollup"><i class="fa fa-angle-up fa-3x"></i></a>
+				</div>
+			</div>
+		</div>		
+	</footer>
+   
+	
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->	
+    <script src="js/jquery-2.1.1.min.js"></script>	
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script src="js/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="js/jquery.easing.1.3.js"></script>
+	<script src="js/jquery.bxslider.min.js"></script>
+	<script src="js/jquery.prettyPhoto.js"></script>
+	<script src="js/jquery.isotope.min.js"></script> 
+	<script src="js/functions.js"></script>
+	<script>
+	wow = new WOW(
+	 {
+	
+		}	) 
+		.init();
+	</script>
+	
+  </body>
+</html>
